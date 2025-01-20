293715316c4259b3e087eb95c945e7401219a18a
kettle12110 <kettle12110@gmail.com>
2025-01-21 00:01:19 +0900

    GitHub Pagesへのデプロイメント設定を追加し、astro.config.mjsを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kettle12110.github.io',
+  base: '/astro-test',
+})
