2ae303236e803121b6721707d1a8fa2c473537d3
kettle12110 <kettle12110@gmail.com>
2025-02-06 00:37:10 +0900

    サイトURLを更新し、メンバーカードコンポーネントにSVGアイコンを追加しました。また、メンバー画像のインポート方法を修正しました。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://kettle12110.github.io',
-  base: '/astro-test',
+  site: 'https://itstudy-jp.github.io/it-study-server-website',
 })
